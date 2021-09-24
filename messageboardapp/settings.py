ed628f646752f13a063822fbf4434f2361a8f40b
KeganBergeron <kegan.bergeron@snhu.edu>
2021-09-24 13:44:31 -0400

    MessageBoardAppWithTests+Heroku

--- messageboardapp/settings.py
+++ messageboardapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4x-dqyoo#oe&_92j=_ra+!r+_u969pz8w&*c1z&m$y_h7u@y_0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
