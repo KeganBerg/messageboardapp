7ede9ccb6aaa30659898edba891558b547d2cc6c
KeganBergeron <kegan.bergeron@snhu.edu>
2021-09-24 12:41:35 -0400

    MessageBoardApp

--- messageboardapp/urls.py
+++ messageboardapp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include  # new
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts.urls')),  # new
 ]
--- posts/models.py
+++ posts/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Post(models.Model):
     text = models.TextField()
 
+    def __str__(self):
+        return self.text[:50]
+
+
 # Create your models here.
